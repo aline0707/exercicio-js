58376ba4070b5d9eaf10b9295c6aa83a22dba972
aline0707 <alinealicate25@gmail.com>
2025-09-11 22:20:14 -0300

    Atividade 001 finalizado

---
+++ Exercicio001/app.js
@@ -0,0 +1,8 @@
+let nome ="", idade = 0
+nome = prompt("informe seu nome: ")
+idade = prompt("informe sua idade: ")
+alert("Olá, seja bem vindo(a) " + nome + " \nVocê tem" + idade + " anos ")
+
+console.log("Olá, seja bem vindo(a) " + nome + " \nVocê tem" + idade + " anos ")
+
+document.write("Olá, seja bem vindo(a) " + nome + " <br>Você tem " + idade + " anos ")
