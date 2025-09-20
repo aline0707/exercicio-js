941f7060e1077d8c2644675512fb4efa42fd788f
aline0707 <alinealicate25@gmail.com>
2025-09-19 22:28:59 -0300

    Exercicio 007

---
+++ Exercicio 007/app.js
@@ -0,0 +1,24 @@
+let dataAtual= new Date()
+console.log(dataAtual.getDay())
+
+let diaDaSemana = dataAtual.getDay()
+
+if(dataAtual.getDay() == 0){
+    document.getElementById("resposta").innerText = "Domingo"
+
+}else if(diaDaSemana == 1){
+    document.getElementById("resposta").innerText = "Segunda-feira"
+
+ } else if(diaDaSemana == 2){
+    document.getElementById("resposta").innerText = "Terça-feira"
+
+}else if(diaDaSemana == 3){
+    document.getElementById("resposta").innerText = "Quarta-feira"
+
+}else if(diaDaSemana == 4){
+    document.getElementById("resposta").innerText = "Quinta-feira"
+}else if(diaDaSemana == 5){
+    document.getElementById("resposta").innerText = "Sexta-feira"
+}else if(diaDaSemana == 6){
+    document.getElementById("resposta").innerText = "Sabado"
+}
