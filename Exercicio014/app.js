4b08cf1aa176007d416da0feef589807a28632b1
aline0707 <alinealicate25@gmail.com>
2025-10-09 20:39:48 -0300

    Exercicio014

--- Exercicio014/app.js
+++ Exercicio014/app.js
@@ -1,22 +1,51 @@
-const timesBrasileirao2025 = [
-  "Atlético Mineiro",
-  "Atlético Paranaense",
-  "Bahia",
-  "Botafogo",
-  "Bragantino",
-  "Corinthians",
-  "Coritiba",
-  "Cruzeiro",
-  "Cuiabá",
-  "Flamengo",
-  "Fluminense",
-  "Fortaleza",
-  "Grêmio",
-  "Internacional",
-  "Juventude",
-  "Palmeiras",
-  "Santos",
-  "São Paulo",
-  "Vasco da Gama",
-  "Vitória"
+// const timesBrasileirao2025 = [
+//   "Atlético Mineiro",
+//   "Atlético Paranaense",
+//   "Bahia",
+//   "Botafogo",
+//   "Bragantino",
+//   "Corinthians",
+//   "Coritiba",
+//   "Cruzeiro",
+//   "Cuiabá",
+//   "Flamengo",
+//   "Fluminense",
+//   "Fortaleza",
+//   "Grêmio",
+//   "Internacional",
+//   "Juventude",
+//   "Palmeiras",
+//   "Santos",
+//   "São Paulo",
+//   "Vasco da Gama",
+//   "Vitória"
+// ];
+
+// console.dir(times)
+const timesECidades = [
+  { nome: "Atlético Mineiro", cidade: "Belo Horizonte - MG" },
+  { nome: "Athletico Paranaense", cidade: "Curitiba - PR" },
+  { nome: "Bahia", cidade: "Salvador - BA" },
+  { nome: "Botafogo", cidade: "Rio de Janeiro - RJ" },
+  { nome: "Bragantino", cidade: "Bragança Paulista - SP" },
+  { nome: "Corinthians", cidade: "São Paulo - SP" },
+  { nome: "Coritiba", cidade: "Curitiba - PR" },
+  { nome: "Cruzeiro", cidade: "Belo Horizonte - MG" },
+  { nome: "Cuiabá", cidade: "Cuiabá - MT" },
+  { nome: "Flamengo", cidade: "Rio de Janeiro - RJ" },
+  { nome: "Fluminense", cidade: "Rio de Janeiro - RJ" },
+  { nome: "Fortaleza", cidade: "Fortaleza - CE" },
+  { nome: "Grêmio", cidade: "Porto Alegre - RS" },
+  { nome: "Internacional", cidade: "Porto Alegre - RS" },
+  { nome: "Juventude", cidade: "Caxias do Sul - RS" },
+  { nome: "Palmeiras", cidade: "São Paulo - SP" },
+  { nome: "Santos", cidade: "Santos - SP" },
+  { nome: "São Paulo", cidade: "São Paulo - SP" },
+  { nome: "Vasco da Gama", cidade: "Rio de Janeiro - RJ" },
+  { nome: "Vitória", cidade: "Salvador - BA" }
 ];
+function fnListaTimes(){
+    document.getElementById("lista_times").innerHTML = 
+    <div class ="col-6 border text_center">${times[0]} -${cidadesTimes[0]}</div> 
+}
+fnListaTimes()
